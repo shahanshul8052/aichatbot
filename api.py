91eeadf561c53f224b4533b0b8372353df811b16
shahanshul005 <85135432+shahanshul005@users.noreply.github.com>
2024-12-23 20:04:22 -0600

    player query filter

--- api.py
+++ api.py
@@ -18,3 +18,36 @@ def query_db(query, args=(), one=False):
     conn.close()
     # Return results
     return (rows[0] if rows else None) if one else rows
+
+
+# endpoint to get all players
+@app.route("/players", methods=["GET"])
+
+def get_players():
+    position = request.args.get("position") # query param for MID, DEF, FWD, GK
+    budget = request.args.get("budget") # query param for budget such as 8.0, 8.5, etc
+
+    # Query to get players based on position and budget
+    query = "SELECT * FROM Players"
+    args = []
+
+
+    # if query params provided, then add filters
+
+    if position or budget:
+        query += " WHERE"
+        # Add filters based on query parameters
+        if position:
+            query += " position = ?"
+            args.append(position)
+        
+        # Add budget filter
+        if budget:
+            if position:
+                query += " AND"
+            query += " cost <= ?"
+            args.append(budget)
+    
+    # Execute query
+    players = query_db(query, args)
+    return jsonify([dict(player) for player in players])
